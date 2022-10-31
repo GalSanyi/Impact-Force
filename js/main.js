25c8f367db8b7efd181b768c155b6a62cdad857c
GalSanyi <86233181+GalSanyi@users.noreply.github.com>
2022-10-31 11:01:52 +0100

    add main slider for mob

--- js/main.js
+++ js/main.js
@@ -4,13 +4,11 @@
     burger.classList.toggle("burger_active");
   });
 })();
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches(".burger")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
