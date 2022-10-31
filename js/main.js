0e77d8817000f55cbe13d4b1c9788815b84e661e
GalSanyi <86233181+GalSanyi@users.noreply.github.com>
2022-10-31 02:12:35 +0100

    burger add

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function () {
+  const burger = document.querySelector(".burger");
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("burger_active");
+  });
+})();
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".burger")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
